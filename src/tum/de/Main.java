7a5db6ede03cb4e9d18ae69025d270aeabda1e33
Santiago Berrezueta <>
2021-11-14 22:21:48 +0100

    Codigo Template

--- src/tum/de/Main.java
+++ src/tum/de/Main.java
@@ -4,11 +4,9 @@ package tum.de;
 public class Main {
 
     public static void main(String[] args) {
-        SavingsAccount savings = new SavingsAccount(4221, 5028.45, 0.02);
+        SavingsAccount savings = new SavingsAccount(4321, 5028.45, 0.02);
         BonusSaverAccount bonusSaver = new BonusSaverAccount(6543, 1475.85, 0.02);
         CheckingAccount checking = new CheckingAccount(9876, 269.93, savings);
-        //Challenge2
-        CreditCardAccount creditCard = new CreditCardAccount(7391,300.0, 1000.00);
 
         savings.deposit(148.04);
         bonusSaver.deposit(41.52);
@@ -16,15 +14,20 @@ public class Main {
         bonusSaver.withdraw(120.38);
         checking.withdraw(320.18);
 
+        //To test the challenges
+        //CreditCardAccount creditCard = new CreditCardAccount(7391,300.0, 1000.00);
 
-        creditCard.pay(532.45);
-        creditCard.pay(467.54);
-        creditCard.pay(0.01);
-        creditCard.pay(0.01);
-        creditCard.compensate();
-        creditCard.pay(23.01);
-        creditCard.pay(532.45);
-        creditCard.handleOverdraftInterest();
+
+
+
+        //creditCard.pay(532.45);
+        //creditCard.pay(467.54);
+        //creditCard.pay(0.01);
+        //creditCard.pay(0.01);
+        //creditCard.compensate();
+        //creditCard.pay(23.01);
+        //creditCard.pay(532.45);
+        //creditCard.handleOverdraftInterest();
 
     }
 }
